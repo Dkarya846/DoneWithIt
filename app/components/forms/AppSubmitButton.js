b4acd280e042116ba57cd8696d7d7e65043dd993
Deepak Kumar <dkarya.delhi.uk@gmail.com>
2022-06-08 04:39:15 +0530

    Added submit button

--- app/components/forms/AppSubmitButton.js
+++ app/components/forms/AppSubmitButton.js
@@ -4,8 +4,8 @@ import { useFormikContext } from "formik";
 import AppButton from "../AppButton";
 
 const AppSubmitButton = ({ title }) => {
-    const { handleSubmit } = useFormikContext();
-    return <AppButton title={title} onPress={handleSubmit} />;
+   const { handleSubmit } = useFormikContext();
+   return <AppButton title={title} onPress={handleSubmit} />;
 };
 
 export default AppSubmitButton;
